9161d55e047e78973f09e93dd240acabb89c9e7d
hwang9u <hwang9u@gmail.com>
2023-06-06 18:22:27 +0000

    Fix bug

--- source/train.py
+++ source/train.py
@@ -27,10 +27,11 @@ FM_LAMBDA=10
 lr = 2e-4
 beta1 = .5
 beta2 = .999
+lr_decay_epochs=3
 
 # DATASET
 guitarset = Hum2GuitarSet(config.guitar_dir, input_type='guitar', stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs)
-hummingset = Hum2GuitarSet(config.guitar_dir, input_type='humming', stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs)
+hummingset = Hum2GuitarSet(config.humming_dir, input_type='humming', stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs)
 h_s,x_s, db_mm=hummingset[0]
 h_s = h_s.unsqueeze(0).to(device)
 x_s = x_s.unsqueeze(0).to(device)
@@ -43,12 +44,25 @@ train_dloader = DataLoader(guitarset, batch_size=1,shuffle=True, num_workers=4 i
 
 global_trainer = Trainer(config, train_dloader=train_dloader, stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs, stage="global", device=device)
 global_trainer.create_optimizer(lr = lr, beta1=beta1, beta2=beta2)
-global_trainer.create_scheduler()
-global_trainer.train(h_s=h_s, x_s=x_s, FM_LAMBDA=FM_LAMBDA)
+global_trainer.create_scheduler(lr_decay_epochs=lr_decay_epochs)
 
+if config.pretrained_local is not None:
+    pass
+else:        
+    if config.pretrained_global is not None:
+        printlog(f'\n[STAGE 1] Load Pre-trained Global Generator/Discriminator from {global_trainer.config.pretrained_global}', global_trainer.LOG_PATH)
+        global_trainer.load_checkpoint(config.pretrained_global)
+    print("[Start] Training Global Generator\n" )
+    global_trainer.train(h_s=h_s, x_s=x_s, FM_LAMBDA=FM_LAMBDA)
+    print("[End] Training Global Generator\n" )
 
-local_trainer =  Trainer(config, train_dloader=train_dloader, stage="local", stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs, device = device)
+print("[Start] Training Local Enhancer\n" )
+global_generator = global_trainer.net_G
+local_trainer =  Trainer(config, train_dloader=train_dloader, global_generator=global_generator, stage="local", stft_kwargs=stft_kwargs, mel_kwargs=mel_kwargs, device = device)
 local_trainer.create_optimizer(lr = lr, beta1=beta1, beta2=beta2)
-local_trainer.create_scheduler()
+local_trainer.create_scheduler(lr_decay_epochs=lr_decay_epochs)
+if config.pretrained_local is not None:
+    printlog(f'\n[STAGE 2] Load Pre-trained Local Enhancer/Discriminator from {config.pretrained_local}', local_trainer.LOG_PATH)
+    local_trainer.load_checkpoint(config.pretrained_local)
 local_trainer.train(h_s=h_s, x_s=x_s)
 
